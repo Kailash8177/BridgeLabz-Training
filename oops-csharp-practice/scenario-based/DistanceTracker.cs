2ca64493b7a45d906432a6dd16e3d6eae20c8bfc
Kailash Nath Yadav <your_email@example.com>
2026-01-02 11:27:45 +0530

    [Kailash Nath Yadav] Add .Added LuckyDraw DistanceTracker Done

---
+++ oops-csharp-practice/scenario-based/DistanceTracker.cs
@@ -0,0 +1,49 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgeLabzTraining
+//{
+//    internal class DistanceTracker
+//    {
+//        public static void Main(string[] args)
+//        {
+//            DistanceTracker tracker = new DistanceTracker();
+//            tracker.TrackDistance();
+
+//        }
+//        public void TrackDistance()
+//        {
+//            int totalDistance = 0;
+//            int stopDistance = 5;
+
+//            string choice;
+
+//            Console.WriteLine("Bus Route Distance Tracker Started");
+
+//            while (true)
+//            {
+//                totalDistance += stopDistance;
+
+//                Console.WriteLine("Bus reached next stop.");
+//                Console.WriteLine("Distance covered so far: " + totalDistance + " km");
+
+//                Console.Write("Do you want to get off at this stop? (yes/no): ");
+//                choice = Console.ReadLine().ToLower();
+
+//                if (choice == "yes")
+//                {
+//                    Console.WriteLine("Passenger got off the bus.");
+//                    break;
+//                }
+//                else if (choice != "no" && choice != "yes")
+//                {
+//                    Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+//                }
+//            }
+
+//            Console.WriteLine("Total distance traveled: " + totalDistance + " km");
+//            Console.WriteLine("Journey Ended ");
+//        }
+//    }
+//}
---
+++ oops-csharp-practice/scenario-based/LuckyDraw.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining
+{
+    internal class LuckyDraw
+    {
+        public static void Main() 
+        {
+            
+            LuckyDraw ld = new LuckyDraw();
+            ld.StartLuckyDraw();
+
+        }
+        public void StartLuckyDraw() 
+        {
+
+            Console.Write("Enter number of visitors: ");
+            int visitors = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= visitors; i++)
+            {
+                Console.Write($"Enter number for Visitor {i}: ");
+                int input = int.Parse(Console.ReadLine());
+
+                if ( input <= 0)
+                {
+                    Console.WriteLine("Invalid input! Skipping this visitor.");
+                    continue;
+                }
+
+                
+                if (input % 3 == 0 && input % 5 == 0)
+                {
+                    Console.WriteLine("Congratulations! You won a gift!");
+                }
+                else
+                {
+                    Console.WriteLine("Better luck next time!");
+                }
+            }
+
+            Console.WriteLine("Lucky Draw Completed 🎊");
+
+
+        }
+    }
+}
