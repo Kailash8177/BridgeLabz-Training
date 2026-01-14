873fa9dc7a0d6b4ef01115adb4c2625bc8785868
Kailash Nath Yadav <your_email@example.com>
2026-01-14 12:35:33 +0530

    [Kailash Nath Yadav] UC1 Done

---
+++ oops-csharp-practice/scenario-based/address-book-system/ContactsMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace address_book_system
+{
+    internal class ContactsMain
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System");
+        }
+    }
+}
