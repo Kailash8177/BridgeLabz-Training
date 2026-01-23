1b5c00d25002b492a20f6b994923b0d904a36d2f
Kailash Nath Yadav <your_email@example.com>
2026-01-23 23:30:45 +0530

    [Kailash Nath Yadav] Add .Added Exception done

---
+++ collections-csharp-practice/gcr-codebase/csharp-exception/ArrayException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class ArrayExceptionDemo
+    {
+        static void Main()
+        {
+            try
+            {
+                int[] numbers = { 10, 20, 30 };
+
+                Console.Write("Enter index: ");
+                int index = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Value at index {index}: {numbers[index]}");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid index!");
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Array is not initialized!");
+            }
+        }
+    }
+}
